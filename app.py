53f217a77f65a3cb090ff41b984f611f0b924158
Long Nguyen <long.vnguyenn@gmail.com>
2018-08-07 15:55:52 -0400

    add return statuses for case when all containers are down and some are down

--- app.py
+++ app.py
@@ -18,18 +18,20 @@ class JsonCollector(object):
         pods = response['items']
 
         # Parse result and expose metrics
-        shipshift_status = 1
+        num_container = 0
+        num_container_down = 0
         for pod in pods:
             pod_name = pod['metadata']['name']
             containers = pod['status']['containerStatuses']
             # Expose status for every container in the pod
             for container in containers:
+                num_container += 1
                 state = container['state']
                 container_name = container['name']
                 if ('running' in state):
                     current_val = 1
                 else:
-                    shipshift_status = 0
+                    num_container_down += 1
                     current_val = 0
                 metric_name = '{}_{}_status'.format(pod_name,container_name)
                 metric_description = 'Current status of container {} in pod {}'.format( container_name,
@@ -39,6 +41,13 @@ class JsonCollector(object):
                 yield metric
 
         # Shipshift up/down status
+        if (num_container_down == 0):
+            shipshift_status = 1
+        elif (num_container_down == num_container):
+            shipshift_status = 0
+        else:
+            shipshift_status = 0.5
+
         metric = Metric('Shipshift_status', 'Current status of Shipshift', 'summary')
         metric.add_sample('Shipshift_status', value=shipshift_status, labels={})
         yield metric
